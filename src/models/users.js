dbbcd70f605c245c8d78b74472f09429dd083921
Pranav Pande <pranav.pande@westpac.com.au>
2019-12-03 13:11:11 +0530

    Added login functionality

--- src/models/users.js
+++ src/models/users.js
@@ -10,6 +10,7 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
+        unique: true,
         required: true,
         trim: true,
         lowercase: true,
@@ -40,6 +41,20 @@ const userSchema = new mongoose.Schema({
         }
     }
 })
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+    if (!user) {
+        throw new Error('Unable to login!')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) {
+        throw new Error('Unable to login!')
+    }
+
+    return user
+}
 
 // This is known as middleware in mongoose. Provides us with hooks
 // to run before and after mongoose operations. We need to create
